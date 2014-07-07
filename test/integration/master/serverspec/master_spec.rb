cbbf89376cedcd97cf9225c2918e37ce48931d00
BK Box <bk.box@rackspace.com>
2014-07-07 10:19:36 -0500

    Fix rubocop error

--- test/integration/master/serverspec/master_spec.rb
+++ test/integration/master/serverspec/master_spec.rb
@@ -2,9 +2,9 @@
 
 require_relative 'spec_helper'
 
-mysql_query = %Q[mysql -uroot -pilikerandompasswords -B --disable-column-names\
-                 -e 'select host from user where user="replicant"' mysql]
+mysql_query = %Q(mysql -uroot -pilikerandompasswords -B --disable-column-names\
+                 -e 'select host from user where user="replicant"' mysql)
 
 describe command(mysql_query) do
-  it { should return_stdout /192\.168\.0\.23/ }
+  it { should return_stdout(/192\.168\.0\.23/) }
 end
