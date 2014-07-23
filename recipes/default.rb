45a10741a03218a993c790c0917ae2a3249a2725
Christopher Coffey <christopher.coffey@rackspace.com>
2014-07-23 16:00:19 -0500

    corrected attribute mis-match

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,6 +23,8 @@ include_recipe 'apt' if node.platform_family?('debian')
 
 include_recipe 'chef-sugar'
 
+include_recipe 'mysql::server'
+ 
 mysql_service 'default'
 
 # creates unique serverid via ipaddress to an int
