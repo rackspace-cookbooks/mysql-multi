35b7453b2eae0f8e94716ea390dc4ff7d1136650
Matthew Thode <mthode@mthode.org>
2014-09-09 12:35:22 -0500

    fixing tests

--- spec/solo/mysql_master_spec.rb
+++ spec/solo/mysql_master_spec.rb
@@ -6,11 +6,12 @@ describe 'mysql-multi::mysql_master' do
   let(:chef_run) do
     ChefSpec::Runner.new do |node|
       node.set['mysql-multi']['slaves'] = ['1.2.3.4']
+      node.set['mysql-multi']['server_repl_password'] = 'foobar'
     end.converge(described_recipe)
   end
 
   let(:grant_content) do
-    "GRANT REPLICATION SLAVE ON *.* TO 'replicant'@'1.2.3.4' IDENTIFIED BY '';
+    "GRANT REPLICATION SLAVE ON *.* TO 'replicant'@'1.2.3.4' IDENTIFIED BY 'foobar';
 FLUSH PRIVILEGES;"
   end
 
