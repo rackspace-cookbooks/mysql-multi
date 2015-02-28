2b1f9ed521ca5ba548f55257554c4da3a69cd08c
Brint O'Hearn <brint.ohearn@rackspace.com>
2015-02-27 18:15:37 -0600

    Updates for master test suite for new recipes

--- test/integration/master/serverspec/master_spec.rb
+++ test/integration/master/serverspec/master_spec.rb
@@ -3,18 +3,18 @@
 require_relative 'spec_helper'
 
 mysql_query = %(mysql -uroot -pilikerandompasswords -B --disable-column-names\
-                 -e 'select host from user where user="replicant"' mysql)
+                 -e 'select host from user where user="replicant"' mysql\
+                 -h localhost --protocol=tcp)
 
 describe command(mysql_query) do
-  it { should return_stdout(/192\.168\.0\.23/) }
+  its(:stdout) { should match /192\.168\.0\.23/ }
 end
 
-describe file('/etc/mysql/conf.d/master.cnf') do
+log_bin = '/var/log/mysql-chef/mysql-bin.log'
+
+describe file('/etc/mysql-chef/conf.d/replication.cnf') do
   it { should contain('sync_binlog = 1').after(/^\[mysqld\]/) }
   it { should contain('binlog-format = mixed').after(/^\[mysqld\]/) }
-  it { should contain('log-bin = mysql-bin').after(/^\[mysqld\]/) }
-end
-
-describe file('/etc/mysql/conf.d/my.cnf') do
-  it { should contain('server_id').from(/^\[mysqld\]/).to(/^\[mysqldump\]/) }
+  it { should contain("log_bin = #{log_bin}").from(/^\[mysqld\]/).to(/^\[/) }
+  it { should contain('server-id').from(/^\[mysqld\]/).to(/^\[/) }
 end
