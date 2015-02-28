b0cefc8d1a8108f926b422dcb5f4c76e0afe409a
Brint O'Hearn <brint.ohearn@rackspace.com>
2015-02-27 18:00:42 -0600

    Updating spec helpers to work with version 2+

--- test/integration/slave/serverspec/spec_helper.rb
+++ test/integration/slave/serverspec/spec_helper.rb
@@ -1,11 +1,4 @@
 # Encoding: utf-8
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/bin'
-  end
-end
+set :backend, :exec
