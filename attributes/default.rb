9be47ffed4b338d09eaafd6c693cca531a9adae9
Christopher Coffey <christopher.coffey@rackspace.com>
2015-04-10 13:05:38 -0500

    changlog and default attribute changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 default['mysql-multi']['master'] = nil
-default['mysql-multi']['slaves'] = %w()
+default['mysql-multi']['slaves'] = nil
 default['mysql-multi']['slave_user'] = 'replicant'
 default['mysql-multi']['server_repl_password'] = nil
 default['mysql-multi']['bind_ip'] = nil
