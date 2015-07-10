16fea2e417b0ed6f3d088338552392ffd90e5e4d
Christopher Coffey <christopher.coffey@rackspace.com>
2015-07-10 14:09:37 -0500

    version bump post release

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description 'MySQL replication wrapper cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/rackspace-cookbooks/mysql-multi'
 issues_url 'https://github.com/rackspace-cookbooks/mysql-multi/issues'
-version '2.1.6' # bump this AFTER release, not in a PR or before
+version '2.1.7' # bump this AFTER release, not in a PR or before
 
 supports 'ubuntu'
 supports 'centos'
